813773b8b875dc4946bf3de7375584f40c628715
Darth Vader <superuser@super.good>
2019-05-28 12:48:52 +0300

    pypi

---
+++ setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+
+    long_description = fh.read()
+
+setuptools.setup(
+
+     name='pypi-test',  
+
+     version='0.1',
+
+     scripts=['pypi-test'] ,
+
+     author="Andre",
+
+     author_email="fatalwithin@gmail.com",
+
+     description="PyPi private repo test",
+
+     long_description=long_description,
+
+   long_description_content_type="text/markdown",
+
+     url="https://github.com/fatalwithin/pypi-test",
+
+     packages=setuptools.find_packages(),
+
+     classifiers=[
+
+         "Programming Language :: Python :: 3",
+
+         "License :: OSI Approved :: MIT License",
+
+         "Operating System :: OS Independent",
+
+     ],
+
+ )
